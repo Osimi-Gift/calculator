49129da95d92f0f5bb27405922dd67c46ef98f73
GBUBEMI <105184327+Osimi-Gift@users.noreply.github.com>
2022-06-21 21:54:22 +0100

    Initial commit

---
+++ calculator.js
@@ -0,0 +1,19 @@
+var opr = prompt (" what Operator do you want to perform e.g '+' '-' '/' '*' ")
+var num1 = parseInt(prompt("Number 1: "))
+var num2 =parseInt(prompt("Number 2: "))
+
+
+
+if(opr == "+" )
+{
+    alert( num1 + num2)  
+}  else if(opr == "-")
+{
+    alert(num1 - num2 )
+} else if(opr == "*")
+{
+    alert(num1 * num2  )
+} else if(opr == "/")
+{
+    alert(num1 / num2  )
+}
